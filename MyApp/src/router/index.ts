b70f48f4aa70b6b365da3b2aaf4cd434980b1d10
cabriniverren <cabriniverren@gmail.com>
2024-11-23 18:16:30 +0100

    Survey page

--- MyApp/src/router/index.ts
+++ MyApp/src/router/index.ts
@@ -1,22 +1,28 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue';
+import SurveyPage from '../views/SurveyPage.vue'; 
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/home', // Redirect to homepage initially
   },
   {
     path: '/home',
     name: 'Home',
-    component: HomePage
-  }
-]
+    component: HomePage, // Home page route (could be after login)
+  },
+  {
+    path: '/survey',
+    name: 'Survey',
+    component: SurveyPage, // Main survey page route
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
